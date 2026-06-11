623e7815e60b18c8c04cd113222b56d9613e3875
Natinou.dev <155118603+Natinou12@users.noreply.github.com>
2026-06-10 21:45:27 -0300

    navbar

--- ex022/src/App.jsx
+++ ex022/src/App.jsx
@@ -1,11 +1,12 @@
 import { Outlet } from "react-router-dom"
+import Navbar from "./componentes/Navbar"
 
 function App() {
 
 
   return (
     <>
-      <p>Navbar</p>
+      <Navbar />
       <h1>React Router</h1>
       <Outlet />
       <p>Footer</p>
--- ex022/src/componentes/Navbar.jsx
+++ ex022/src/componentes/Navbar.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const Navbar = () => {
   return (
     <nav>
-        
+        <Link to="/">Home</Link> <br />
+        <Link to="/contatos">Contatos</Link>
     </nav>
   )
 }
